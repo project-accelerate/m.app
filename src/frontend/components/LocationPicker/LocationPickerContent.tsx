327ff5d9318ef9ab238ee4edf1fc6fa05dfdb724
Chris Devereux <devereux.chris@gmail.com>
2018-05-25 23:50:33 +0100

    Add LocationPicker component

---
+++ src/frontend/components/LocationPicker/LocationPickerContent.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react'
+import { withStyles, InputLabel, FormControl, Grid, Input, IconButton, Button, FormHelperText } from '@material-ui/core';
+import { Search, MyLocation } from '@material-ui/icons';
+
+interface LocationPickerContentProps {
+  /** Current outcode/postcode */
+  value: string
+
+  /** Fired when edited postcode changes */
+  onChange: React.ChangeEventHandler<HTMLInputElement>
+  
+  /** Fired to request commiting the edited postcode */
+  onSubmit: React.FormEventHandler<{}>
+
+  /** Fired to request using the user's location */
+  onRequestGeolocation: () => void
+
+  /** True if the postcode is invalid */
+  error: boolean
+}
+
+const styles = withStyles(({ spacing }) => ({
+  container: {
+    margin: spacing.unit
+  },
+  control: {
+    margin: spacing.unit
+  }
+}))
+
+/** 
+ * View layer of LocationPicker
+ */
+export const LocationPickerContent = styles<LocationPickerContentProps>(({ classes, value, error, onChange, onSubmit, onRequestGeolocation }) => (
+  <form className={classes.container} onSubmit={onSubmit}>
+    <Grid container alignItems="center" direction="row">
+      <FormControl className={classes.control}>
+        <InputLabel>Postcode</InputLabel>
+        <Input
+          value={value}
+          onChange={onChange}
+          inputProps={{ size: 8 }}
+          error={error}
+          endAdornment={
+            <IconButton onClick={onSubmit}>
+              <Search />
+            </IconButton>
+          }
+        />
+        {error && <FormHelperText>Invalid Postcode</FormHelperText>}
+      </FormControl>
+      <Button variant="raised" color="primary" className={classes.container} size="small">
+        <MyLocation />&nbsp;Use My Location
+      </Button>
+    </Grid>
+  </form>
+))
---
+++ src/frontend/components/LocationPicker/geolocation.ts
@@ -0,0 +1,25 @@
+import { postcodesUrl } from "../../config/properties";
+
+/** Return the user's current geolocation */
+export function getUserPosition() {
+  return new Promise<Position>((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
+}
+
+export interface PositionInfo {
+  /** Nearest postcode */
+  postcode: string
+
+  /** Outcode (first portion) of nerest postcode */
+  outcode: string
+}
+
+/** Lookup information about a geolocated position */
+export function getPositionInfo({ coords: { latitude, longitude } }: Position): Promise<PositionInfo | undefined> {
+  const url = `${postcodesUrl}/postcodes?lon=${longitude}&lat=${latitude}`
+  
+  return fetch(url)
+    .then(response => response.json())
+    .then(({ result }) => result)
+}
