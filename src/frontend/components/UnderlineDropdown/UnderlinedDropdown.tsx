e8c025bdf3653464021b79cf72e090a854bb5d2f
Chris Devereux <devereux.chris@gmail.com>
2018-05-25 23:50:50 +0100

    Add UnderlinedDropdown component

---
+++ src/frontend/components/UnderlineDropdown/UnderlinedDropdown.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react'
+import { Popover } from '@material-ui/core';
+
+interface UnderlinedDropdownProps<T> {
+  value: T
+  onChange?: (x: T) => void
+}
+
+interface UnderlinedDropdownRenderProps<T> {
+  value: T
+  onChange: (x: T) => void
+}
+
+interface UnderlinedDropdownConfig<T> {
+  format: (x: T) => React.ReactNode
+  render: (props: UnderlinedDropdownRenderProps<T>) => React.ReactNode
+}
+
+export function createUnderlinedDropdown<T>({ render, format }: UnderlinedDropdownConfig<T>) {
+  return class UnderlinedDropdown extends React.Component<UnderlinedDropdownProps<T>, { visible: boolean }> {
+    state = { visible: false }
+    anchor?: HTMLAnchorElement
+
+    handleClick: React.MouseEventHandler<HTMLAnchorElement> = (event) => {
+      this.anchor = event.currentTarget
+      event.preventDefault()
+
+      this.setState({ visible: true })
+    }
+
+    handleClose = () => {
+      this.setState({ visible: false })
+    }
+
+    handleChange = (x: T) => {
+      if (this.props.onChange) {
+        this.props.onChange(x)
+      }
+
+      this.setState({ visible: false })
+    }
+
+    render() {
+      return (
+        <>
+          <a href="#" onClick={this.handleClick}>{format(this.props.value)}</a>
+          <Popover
+            anchorEl={this.anchor}
+            open={this.state.visible}
+            onClose={this.handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+          {
+            render({
+              value: this.props.value,
+              onChange: this.handleChange
+            })
+          }
+          </Popover>
+        </>
+      )
+    }
+  }
+}
---
+++ src/frontend/components/UnderlineDropdown/__tests__/UnderlinedDropdown.test.tsx
@@ -0,0 +1,106 @@
+import * as React from 'react'
+import { shallow, ShallowWrapper } from 'enzyme';
+import { createUnderlinedDropdown } from '../UnderlinedDropdown';
+import { Popover } from '@material-ui/core';
+import { someEvent } from '../../../../test/testUtils';
+
+describe('UnderlinedDropdown', () => {
+  it('renders value', () => {
+    const fixture = new Fixture(
+      <ExampleMenu value={{ content: 'Foo' }} />
+    )
+
+    expect(fixture.valueText).toHaveText("Foo")
+  })
+
+  it('starts undisclosed', () => {
+    const fixture = new Fixture(
+      <ExampleMenu value={{ content: 'Foo' }} />
+    )
+
+    expect(fixture.popover).toHaveProp('open', false)
+  })
+
+  it('discloses on click and renders item with value', () => {
+    const fixture = new Fixture(
+      <ExampleMenu value={{ content: 'Foo' }} />
+    )
+
+    fixture.click()
+
+    expect(fixture.popover).toHaveProp('open', true)
+    expect(fixture.content).toHaveProp('value', { content: 'Foo' })
+  })
+
+  it('hides on change and fires event listener', () => {
+    const onChange = jest.fn()
+    const fixture = new Fixture(
+      <ExampleMenu value={{ content: 'Foo' }} onChange={onChange} />
+    )
+
+    fixture.click()
+    fixture.valueChange({ content: 'Bar' })
+
+    expect(fixture.popover).toHaveProp('open', false)
+    expect(onChange).toHaveBeenCalledWith({ content: 'Bar' })
+  })
+
+  it('hides on dismiss and does not fire event listener', () => {
+    const onChange = jest.fn()
+    const fixture = new Fixture(
+      <ExampleMenu value={{ content: 'Foo' }} onChange={onChange} />
+    )
+
+    fixture.click()
+    fixture.dismiss()
+
+    expect(fixture.popover).toHaveProp('open', false)
+    expect(onChange).not.toHaveBeenCalled()
+  })
+})
+
+const ExampleMenu = createUnderlinedDropdown<ExampleValue>({
+  render: props => <ExampleContent {...props} />,
+  format: value => value.content
+})
+
+interface ExampleValue {
+  content: string
+}
+
+const ExampleContent = (props: { value: ExampleValue, onChange: Function }) => null
+
+class Fixture {
+  tree: ShallowWrapper
+
+  constructor(el: React.ReactElement<{}>) {
+    this.tree = shallow(el)
+  }
+
+  get valueText() {
+    return this.tree.find("a")
+  }
+
+  get content() {
+    return this.tree.find(ExampleContent)
+  }
+
+  get popover() {
+    return this.tree.find(Popover)
+  }
+
+  valueChange(newValue: ExampleValue) {
+    this.content.props().onChange(newValue)
+    this.tree.update()
+  }
+
+  click() {
+    this.valueText.simulate('click', someEvent())
+    this.tree.update()
+  }
+
+  dismiss() {
+    this.popover.props().onClose(someEvent())
+    this.tree.update()
+  }
+}
