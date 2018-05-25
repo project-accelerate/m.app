77f9345318e7e461c106a66ef228c05248bd1641
Chris Devereux <devereux.chris@gmail.com>
2018-05-25 23:51:18 +0100

    Add mock events

--- src/test/testUtils.ts
+++ src/test/testUtils.ts
@@ -1,1 +1,11 @@
 export const someString = () => "Foo"
+
+export const someEvent = (props: any = {}) => ({
+  ...props,
+  preventDefault: jest.fn(),
+  stopPropagation: jest.fn()
+})
+
+export const someEventWithValue = (value: any) => someEvent({
+  currentTarget: { value }
+})
