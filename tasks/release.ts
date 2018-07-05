c682107d85e4d175376eee9048a9b38f4c7df472
Chris Devereux <devereux.chris@gmail.com>
2018-07-05 12:32:42 +0100

    Disable native auto-deploy for now

--- tasks/release.ts
+++ tasks/release.ts
@@ -1,10 +1,5 @@
 import { start } from './start'
-import { publishFrontendNative } from './build'
 
 export function release() {
-  if (process.env.EXPO_RELEASE_CHANNEL) {
-    publishFrontendNative(process.env.EXPO_RELEASE_CHANNEL)
-  }
-
   start('backend', 'release')
 }
