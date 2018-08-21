a3b31df0e7c37c558001c2c40e35ec92956e73ca
Chris Devereux <devereux.chris@gmail.com>
2018-08-21 23:56:30 +0100

    Fake out expo client in integration tests

--- backend/testSetup.integration.ts
+++ backend/testSetup.integration.ts
@@ -1,17 +1,15 @@
-import 'reflect-metadata'
+import './config/environment'
 import { join } from 'path'
 import dotenv from 'dotenv'
-import { configureDb } from './config/db'
 import { Container } from 'typedi'
-import { AMQPConnection } from 'backend/app/common/external/amqp/AMQPConnection'
-import { MockAMQPConnection } from 'backend/app/common/external/amqp/MockAMQPConnection'
 import { ExpoPushClient } from 'backend/app/device/external/ExpoPushClient'
-import { mock } from 'ts-mockito'
+import { FakeExpoPushClient } from 'backend/app/device/external/FakeExpoPushClient'
+import { configureDb } from './config/db'
 
 dotenv.config({ path: join(__dirname, '..', 'config', 'public.env') })
 dotenv.config({ path: join(__dirname, '..', 'config', '.env') })
 
-Container.set(AMQPConnection, new MockAMQPConnection())
-Container.set(ExpoPushClient, mock(ExpoPushClient))
-
-beforeAll(configureDb)
+beforeAll(async () => {
+  Container.set(ExpoPushClient, new FakeExpoPushClient())
+  await configureDb()
+})
