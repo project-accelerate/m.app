afe9f0b91b787bd6599b91e5b0ed690323fb1bc6
Chris Devereux <devereux.chris@gmail.com>
2018-08-24 00:03:16 +0100

    Add debug logging to notifications

--- backend/app/device/external/ExpoPushClient.ts
+++ backend/app/device/external/ExpoPushClient.ts
@@ -1,5 +1,11 @@
-import { ExpoPushMessage, Expo, ExpoPushTicket, ExpoPushReceipt, ExpoPushReceiptId } from 'expo-server-sdk'
-import { Service } from 'typedi';
+import {
+  ExpoPushMessage,
+  Expo,
+  ExpoPushTicket,
+  ExpoPushReceipt,
+  ExpoPushReceiptId,
+} from 'expo-server-sdk'
+import { Service } from 'typedi'
 import log from 'winston'
 
 @Service()
@@ -11,16 +17,20 @@ export class ExpoPushClient {
     const tickets: ExpoPushTicket[] = []
 
     if (chunks.length > 1) {
-      log.warn(`Push notification batch exceeds a single chunk (chunks: ${chunks.length}, chunkSize: ${chunks[0].length})`)
+      log.warn(
+        `[Push] batch exceeds a single chunk (chunks: ${
+          chunks.length
+        }, chunkSize: ${chunks[0].length})`,
+      )
     }
 
     for (let chunk of chunks) {
       try {
-        let ticketChunk = await this.expo.sendPushNotificationsAsync(chunk);
-        tickets.push(...ticketChunk);
-
+        log.debug('[Push] Sending notifications to devices', chunk)
+        let ticketChunk = await this.expo.sendPushNotificationsAsync(chunk)
+        tickets.push(...ticketChunk)
       } catch (error) {
-        log.error(error);
+        log.error(error)
       }
     }
 
@@ -28,20 +38,25 @@ export class ExpoPushClient {
   }
 
   async requestNotificationReceipts(receiptIds: ExpoPushReceiptId[]) {
-    const chunks = this.expo.chunkPushNotificationReceiptIds(receiptIds);
+    const chunks = this.expo.chunkPushNotificationReceiptIds(receiptIds)
     const receipts: Record<string, ExpoPushReceipt> = {}
 
     if (chunks.length > 1) {
-      log.warn(`Push notification receipt batch exceeds a single chunk (chunks: ${chunks.length}, chunkSize: ${chunks[0].length})`)
+      log.warn(
+        `[Push] Receipt batch exceeds a single chunk (chunks: ${
+          chunks.length
+        }, chunkSize: ${chunks[0].length})`,
+      )
     }
 
     for (let chunk of chunks) {
       try {
-        const chunkReceipts = await this.expo.getPushNotificationReceiptsAsync(chunk);
+        const chunkReceipts = await this.expo.getPushNotificationReceiptsAsync(
+          chunk,
+        )
         Object.assign(receipts, chunkReceipts)
-
       } catch (error) {
-        log.error(error);
+        log.error(error)
       }
     }
 
