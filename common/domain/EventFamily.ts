f137aecfc7249a930b9672697771e5995f4c7819
Chris Devereux <devereux.chris@gmail.com>
2018-08-25 10:59:21 +0100

    Adds vote event category

--- common/domain/EventFamily.ts
+++ common/domain/EventFamily.ts
@@ -2,6 +2,7 @@ import { never } from '../util'
 
 export enum EventFamily {
   TWT_2018 = 'TWT_2018',
+  LABOUR_2018_VOTE = 'LABOUR_2018',
   LABOUR_2018 = 'LABOUR_2018',
 }
 
@@ -9,11 +10,15 @@ export const allEventFamilies = Object.keys(EventFamily) as EventFamily[]
 
 export function getEventFamilyName(family: EventFamily): string {
   if (family === EventFamily.TWT_2018) {
-    return 'TWT 2018'
+    return 'TWT'
   }
 
   if (family === EventFamily.LABOUR_2018) {
-    return 'Labour 2018'
+    return 'Labour Conference Event'
+  }
+
+  if (family === EventFamily.LABOUR_2018_VOTE) {
+    return 'Vote'
   }
 
   return never(family)
