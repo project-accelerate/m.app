3b83b2d9d10f6cc0ac8460c597146bee314ba0a1
Chris Devereux <devereux.chris@gmail.com>
2018-08-26 18:25:06 +0100

    Don't load tests when scanning for services

--- backend/util/scanPaths.ts
+++ backend/util/scanPaths.ts
@@ -3,8 +3,12 @@ import { resolve } from 'path'
 
 export function scanPaths(...patterns: string[]): unknown[] {
   return patterns.flatMap(pattern =>
-    glob.sync(pattern).flatMap(file =>
-      Object.values(require(resolve(file)))
-    )
+    glob.sync(pattern).flatMap(file => {
+      if (file.includes('.test.') || file.includes('.integrationTest.')) {
+        return []
+      }
+
+      return Object.values(require(resolve(file)))
+    }),
   )
 }
