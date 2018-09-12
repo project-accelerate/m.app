19193b8f74c3f58ce8b15502732e6d6851dd01e3
Chris Devereux <devereux.chris@gmail.com>
2018-09-12 22:43:06 +0100

    wip

--- frontend/native/app/twt/Home/HomeScreen.tsx
+++ frontend/native/app/twt/Home/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { StyleSheet, Image, View } from 'react-native'
 import * as faker from 'faker'
-import { NavigationScreenOptions } from 'react-navigation'
+import { NavigationScreenOptions, NavigationInjectedProps } from 'react-navigation'
 import twt from './TWTa_hdr.jpg'
 import { theme } from '../../../theme'
 import Logo from '../../../assets/mlogo.png'
@@ -20,6 +20,12 @@ import {
   HEADER_CONTENT_HEIGHT,
 } from '../../common/Screen/HeaderBar'
 import { getStatusBarHeight } from '../../common/platform'
+import { createStateConnector } from '../../../state';
+import { calendar } from '../Calendar/calendarState';
+import { Touchable } from '../../common/Widgets/Widgets';
+import { format, isSameDay } from 'date-fns';
+import { TimeProvider } from '../../common/Time/TimeProvider';
+import { Routes } from '../../../routes';
 
 const style = StyleSheet.create({
   logo: {
@@ -40,8 +46,22 @@ const style = StyleSheet.create({
   },
 })
 
-export class HomeScreen extends React.Component {
+const Connect = createStateConnector(() => ({
+  events: calendar.selectors.upcomingEvents
+}))
+
+export class HomeScreen extends React.Component<NavigationInjectedProps> {
   static navigationOptions: NavigationScreenOptions = {}
+
+  navigateToEvent(e: { id: string, name: string }) {
+    this.props.navigation.navigate({
+      routeName: new Routes().getRoutename('EventDetailScreen'),
+      params: {
+        id: e.id,
+        title: e.name
+      }
+    })
+  }
 
   render() {
     return (
@@ -61,11 +81,31 @@ export class HomeScreen extends React.Component {
         }
       >
         <CardContainer>
-          <CardGroupHeader>Upcoming</CardGroupHeader>
-          <Card>
-            <CardSubheader>Today 11:00</CardSubheader>
-            <CardHeader>{faker.lorem.words(3)}</CardHeader>
-          </Card>
+          <Connect>
+          {
+            ({ events }) => (
+              events.length > 0 && (
+                <View>
+                  <CardGroupHeader>Upcoming</CardGroupHeader>
+                  <TimeProvider granularity="minutes">
+                  {
+                    (time) => (
+                      events.map(e => (
+                        <Touchable key={e.id} onPress={() => this.navigateToEvent(e)}>
+                          <Card>
+                            <CardSubheader>{isSameDay(e, e.startTime) ? 'Today' : format(e.startTime, 'ddd')} {format(e.startTime, 'HH:MM')}</CardSubheader>
+                            <CardHeader>{e.name}</CardHeader>
+                          </Card>
+                        </Touchable>
+                      ))
+                    )
+                  }
+                  </TimeProvider>>
+                </View>
+              )
+            )
+          }
+          </Connect>
           <CardGroupHeader>News</CardGroupHeader>
           {times(10, i => (
             <Card key={i}>
