96f23f362a138957bcf875ff5219fe8ff3a7b144
Chris Devereux <devereux.chris@gmail.com>
2018-09-13 01:11:54 +0100

    Adds offline support

--- frontend/native/config/graphql.ts
+++ frontend/native/config/graphql.ts
@@ -1,9 +1,25 @@
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { persistCache } from 'apollo-cache-persist'
+import { RetryLink } from 'apollo-link-retry'
 import { BACKEND_URL } from './properties'
+import { AsyncStorage } from 'react-native'
+import { ApolloLink } from 'apollo-link'
+
+const SCHEMA_VERSION = 1
+
+const cache = new InMemoryCache()
+persistCache({
+  cache,
+  storage: AsyncStorage as any,
+  key: `graphql-data-v${SCHEMA_VERSION}`,
+})
 
 export const graphQlClient = new ApolloClient({
-  link: new HttpLink({ uri: `${BACKEND_URL}/graphql` }),
-  cache: new InMemoryCache(),
+  link: ApolloLink.from([
+    new RetryLink(),
+    new HttpLink({ uri: `${BACKEND_URL}/graphql` }),
+  ]),
+  cache,
 })
