f0ff31eda1a3979bf52463e86a433fbd6fbed75b
Chris Devereux <devereux.chris@gmail.com>
2018-09-16 02:30:03 +0100

    Adds user update endpoint

--- backend/app/user/web/UserMutations.ts
+++ backend/app/user/web/UserMutations.ts
@@ -1,14 +1,41 @@
-import { ObjectType, Field, Resolver, Mutation, Arg } from 'type-graphql'
+import {
+  ObjectType,
+  Field,
+  Resolver,
+  Mutation,
+  Arg,
+  FieldResolver,
+  Ctx,
+} from 'type-graphql'
+import { UserAdminService } from 'backend/app/user/application/UserAdminService'
+import { GraphQLBoolean } from 'graphql'
+import { MutationRequest } from 'backend/app/common/resolverUtils'
+import { CreateUserRequest, User } from 'backend/app/user/domain/User'
 
 @ObjectType()
 export class UserMutation {
   @Field() id!: string
 }
 
-@Resolver()
+@Resolver(() => UserMutation)
 export class UserMutationResolver {
+  constructor(private userAdminService: UserAdminService) {}
+
   @Mutation()
   mutateUser(@Arg('id') id: string): UserMutation {
     return { id }
+  }
+
+  @FieldResolver(() => User)
+  async update(
+    @Ctx() user: UserMutation,
+    @MutationRequest(() => CreateUserRequest)
+    change: CreateUserRequest,
+  ): Promise<User> {
+    await this.userAdminService.update(user.id, change)
+    return {
+      id: user.id,
+      ...change,
+    }
   }
 }
