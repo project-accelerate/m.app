9b48d4daf121b75bbd03676faad03b9c787d029a
Chris Devereux <devereux.chris@gmail.com>
2018-09-18 20:36:07 +0100

    Adds explicit consent to contact db field

--- backend/app/conference/web/__tests__/ConferenceAttendanceMutations.integrationTest.ts
+++ backend/app/conference/web/__tests__/ConferenceAttendanceMutations.integrationTest.ts
@@ -14,6 +14,7 @@ describe('registerConferenceAttendance mutation', () => {
         user: {
           optedIntoNotifications: true,
           isDelegate: true,
+          consentToContact: true,
         },
       })
 
