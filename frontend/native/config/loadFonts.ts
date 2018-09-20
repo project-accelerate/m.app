be668c9ac3a0413461a09032ac097d7733e0fd37
Chris Devereux <devereux.chris@gmail.com>
2018-09-20 23:37:57 +0100

    Prefetch font

--- frontend/native/config/loadFonts.ts
+++ frontend/native/config/loadFonts.ts
@@ -1,7 +1,9 @@
 import { Font } from 'expo'
+import { FontAwesome } from '@expo/vector-icons'
 
 export function loadFonts() {
   return Font.loadAsync({
+    ...(FontAwesome as any).font,
     'open-sans-bold': require('../assets/fonts/Open_Sans/OpenSans-Bold.ttf'),
     'open-sans-light': require('../assets/fonts/Open_Sans/OpenSans-Light.ttf'),
     'open-sans-semibold': require('../assets/fonts/Open_Sans/OpenSans-SemiBold.ttf'),
