fd0cbee8a187f7d147edcd9ade8225fba50ec41c
Chris Devereux <devereux.chris@gmail.com>
2018-09-22 20:14:02 +0100

    Handle news notification links

--- frontend/native/notifications.ts
+++ frontend/native/notifications.ts
@@ -1,6 +1,7 @@
 import { EventReminderNotificationHandler } from './app/twt/Calendar/EventReminderNotification'
 import { NotificationHandlerType } from './app/common/Notification/NotificationHandler'
+import { NewsHandler } from './app/twt/News/NewsHandler'
 
 export function notificationHandlers(): NotificationHandlerType[] {
-  return [EventReminderNotificationHandler]
+  return [EventReminderNotificationHandler, NewsHandler]
 }
