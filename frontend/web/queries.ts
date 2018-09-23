8407e8be40dcc14476de9ad81834577edb3feb61
Chris Devereux <devereux.chris@gmail.com>
2018-09-23 19:18:42 +0100

    TWT_ONLY notifications

--- frontend/web/queries.ts
+++ frontend/web/queries.ts
@@ -2,373 +2,372 @@
 //  This file was automatically generated and should not be edited.
 
 export interface CreateEventRequest {
-  name: string,
-  speakers: Array< string >,
-  venue: string,
-  startTime: string,
-  endTime: string,
-  introduction: string,
-  detail: string,
-  family: EventFamily,
-  photoUpload?: string | null,
-};
+  name: string
+  speakers: Array<string>
+  venue: string
+  startTime: string
+  endTime: string
+  introduction: string
+  detail: string
+  family: EventFamily
+  photoUpload?: string | null
+}
 
 export enum EventFamily {
-  LABOUR_2018 = "LABOUR_2018",
-  LABOUR_2018_VOTE = "LABOUR_2018_VOTE",
-  TWT_2018 = "TWT_2018",
+  LABOUR_2018 = 'LABOUR_2018',
+  LABOUR_2018_VOTE = 'LABOUR_2018_VOTE',
+  TWT_2018 = 'TWT_2018',
 }
-
 
 export interface EditEventRequest {
-  id: string,
-  name?: string | null,
-  speakers?: Array< string > | null,
-  venue?: string | null,
-  startTime?: string | null,
-  endTime?: string | null,
-  introduction?: string | null,
-  detail?: string | null,
-  family?: EventFamily | null,
-  photoUpload?: string | null,
-};
-
-export enum ConferenceNotificationScope {
-  DELEGATES = "DELEGATES",
-  EVERYONE = "EVERYONE",
+  id: string
+  name?: string | null
+  speakers?: Array<string> | null
+  venue?: string | null
+  startTime?: string | null
+  endTime?: string | null
+  introduction?: string | null
+  detail?: string | null
+  family?: EventFamily | null
+  photoUpload?: string | null
 }
 
+export enum ConferenceNotificationScope {
+  DELEGATES = 'DELEGATES',
+  EVERYONE = 'EVERYONE',
+  TWT_ONLY = 'TWT_ONLY',
+}
 
 export interface ConferenceNotificationSendRequest {
-  title: string,
-  message: string,
-  detail: string,
-  urgent: boolean,
-  link?: string | null,
-  associatedEventId?: string | null,
-  scope: ConferenceNotificationScope,
-};
+  title: string
+  message: string
+  detail: string
+  urgent: boolean
+  link?: string | null
+  associatedEventId?: string | null
+  scope: ConferenceNotificationScope
+}
 
 export interface CreatePersonRequest {
-  name: string,
-  bio?: string | null,
-  twitterHandle?: string | null,
-  photoUpload?: string | null,
-};
+  name: string
+  bio?: string | null
+  twitterHandle?: string | null
+  photoUpload?: string | null
+}
 
 export interface EditPersonRequest {
-  id: string,
-  name: string,
-  bio?: string | null,
-  twitterHandle?: string | null,
-  photoUpload?: string | null,
-};
+  id: string
+  name: string
+  bio?: string | null
+  twitterHandle?: string | null
+  photoUpload?: string | null
+}
 
 export interface CreateVenueRequest {
-  name: string,
-  description: string,
-  photoUpload?: string | null,
-  address: AddressInput,
-};
+  name: string
+  description: string
+  photoUpload?: string | null
+  address: AddressInput
+}
 
 export interface AddressInput {
-  streetAddress: string,
-  city: string,
-  postcode: string,
-};
+  streetAddress: string
+  city: string
+  postcode: string
+}
 
 export interface CreateEventMutationVariables {
-  req: CreateEventRequest,
-};
+  req: CreateEventRequest
+}
 
 export interface CreateEventMutation {
   // Submit a new event
-  createEvent:  {
-    id: string,
-  },
-};
+  createEvent: {
+    id: string
+  }
+}
 
 export interface EditEventMutationVariables {
-  req: EditEventRequest,
-};
+  req: EditEventRequest
+}
 
 export interface EditEventMutation {
   // Edit an event
-  editEvent: boolean,
-};
+  editEvent: boolean
+}
 
 export interface EventAdminPageQuery {
-  venueOptions:  {
-    edges:  Array< {
-      node:  {
-        id: string,
-        name: string,
-      },
-    } >,
-  },
-  speakerOptions:  {
-    edges:  Array< {
-      node:  {
-        id: string,
-        name: string,
-      },
-    } >,
-  },
-  allEvents:  {
-    edges:  Array< {
-      node:  {
-        id: string,
-        name: string,
-        family: EventFamily,
-        speakers:  {
-          edges:  Array< {
-            node:  {
-              id: string,
-              name: string,
-            },
-          } >,
-        },
+  venueOptions: {
+    edges: Array<{
+      node: {
+        id: string
+        name: string
+      }
+    }>
+  }
+  speakerOptions: {
+    edges: Array<{
+      node: {
+        id: string
+        name: string
+      }
+    }>
+  }
+  allEvents: {
+    edges: Array<{
+      node: {
+        id: string
+        name: string
+        family: EventFamily
+        speakers: {
+          edges: Array<{
+            node: {
+              id: string
+              name: string
+            }
+          }>
+        }
         // Return the event venue
-        venue:  {
-          id: string,
-          name: string,
-        },
-        startTime: string,
-        endTime: string,
-        introduction: string,
-        detail: string,
-        photo:  {
-          sourceUrl: string,
-        } | null,
-      },
-    } >,
-  },
-};
+        venue: {
+          id: string
+          name: string
+        }
+        startTime: string
+        endTime: string
+        introduction: string
+        detail: string
+        photo: {
+          sourceUrl: string
+        } | null
+      }
+    }>
+  }
+}
 
 export interface NotificationAdminPageQuery {
-  events:  {
-    edges:  Array< {
-      node:  {
-        id: string,
-        name: string,
-      },
-    } >,
-  },
-  sentNotifications:  {
-    edges:  Array< {
-      node:  {
-        id: string,
-        timeSent: string,
-        title: string,
-        message: string,
-        scope: ConferenceNotificationScope,
-      },
-    } >,
-  },
-};
+  events: {
+    edges: Array<{
+      node: {
+        id: string
+        name: string
+      }
+    }>
+  }
+  sentNotifications: {
+    edges: Array<{
+      node: {
+        id: string
+        timeSent: string
+        title: string
+        message: string
+        scope: ConferenceNotificationScope
+      }
+    }>
+  }
+}
 
 export interface CreateNotificationMutationVariables {
-  req: ConferenceNotificationSendRequest,
-};
+  req: ConferenceNotificationSendRequest
+}
 
 export interface CreateNotificationMutation {
-  sendConferenceNotification: boolean,
-};
+  sendConferenceNotification: boolean
+}
 
 export interface CreatePersonMutationVariables {
-  req: CreatePersonRequest,
-};
+  req: CreatePersonRequest
+}
 
 export interface CreatePersonMutation {
-  createPerson:  {
-    id: string,
-  },
-};
+  createPerson: {
+    id: string
+  }
+}
 
 export interface EditPersonMutationVariables {
-  req: EditPersonRequest,
-};
+  req: EditPersonRequest
+}
 
 export interface EditPersonMutation {
   // Edit an event
-  editPerson: boolean,
-};
+  editPerson: boolean
+}
 
 export interface PersonAdminPageQuery {
-  allPeople:  {
-    edges:  Array< {
-      node:  {
-        id: string,
-        name: string,
-        bio: string | null,
-        photo:  {
-          sourceUrl: string,
-        } | null,
-        twitterHandle: string | null,
-      },
-    } >,
-  },
-};
+  allPeople: {
+    edges: Array<{
+      node: {
+        id: string
+        name: string
+        bio: string | null
+        photo: {
+          sourceUrl: string
+        } | null
+        twitterHandle: string | null
+      }
+    }>
+  }
+}
 
 export interface CreateVenueMutationVariables {
-  req: CreateVenueRequest,
-};
+  req: CreateVenueRequest
+}
 
 export interface CreateVenueMutation {
-  createVenue:  {
-    id: string,
-  },
-};
+  createVenue: {
+    id: string
+  }
+}
 
 export interface VenueAdminPageQuery {
-  allVenues:  {
-    edges:  Array< {
-      node:  {
-        id: string,
-        name: string,
-        description: string,
-        address:  {
-          streetAddress: string,
-          postcode: string,
-          city: string,
-        },
-        photo:  {
-          sourceUrl: string,
-        } | null,
-      },
-    } >,
-  },
-};
+  allVenues: {
+    edges: Array<{
+      node: {
+        id: string
+        name: string
+        description: string
+        address: {
+          streetAddress: string
+          postcode: string
+          city: string
+        }
+        photo: {
+          sourceUrl: string
+        } | null
+      }
+    }>
+  }
+}
 
 export interface EventFeedPageQueryVariables {
-  radiusInMiles: number,
-  postcode: string,
-};
+  radiusInMiles: number
+  postcode: string
+}
 
 export interface EventFeedPageQuery {
   // Get feed of upcoming events for a given locaton
-  eventFeed:  Array< {
-    id: string,
-    name: string,
-    family: EventFamily,
+  eventFeed: Array<{
+    id: string
+    name: string
+    family: EventFamily
     // Return the event venue
-    venue:  {
-      id: string,
-      name: string,
-      address:  {
-        postcode: string,
-      },
-    },
-    startTime: string,
-    endTime: string,
-    introduction: string,
-  } >,
-};
+    venue: {
+      id: string
+      name: string
+      address: {
+        postcode: string
+      }
+    }
+    startTime: string
+    endTime: string
+    introduction: string
+  }>
+}
 
 export interface EventAdminCardFragment {
-  id: string,
-  name: string,
-  family: EventFamily,
-  speakers:  {
-    edges:  Array< {
-      node:  {
-        id: string,
-        name: string,
-      },
-    } >,
-  },
+  id: string
+  name: string
+  family: EventFamily
+  speakers: {
+    edges: Array<{
+      node: {
+        id: string
+        name: string
+      }
+    }>
+  }
   // Return the event venue
-  venue:  {
-    id: string,
-    name: string,
-  },
-  startTime: string,
-  endTime: string,
-  introduction: string,
-  detail: string,
-  photo:  {
-    sourceUrl: string,
-  } | null,
-};
+  venue: {
+    id: string
+    name: string
+  }
+  startTime: string
+  endTime: string
+  introduction: string
+  detail: string
+  photo: {
+    sourceUrl: string
+  } | null
+}
 
 export interface NotificationAdminCardFragment {
-  id: string,
-  timeSent: string,
-  title: string,
-  message: string,
-  scope: ConferenceNotificationScope,
-};
+  id: string
+  timeSent: string
+  title: string
+  message: string
+  scope: ConferenceNotificationScope
+}
 
 export interface NotificationListFragment {
-  edges:  Array< {
-    node:  {
-      id: string,
-      name: string,
-    },
-  } >,
-};
+  edges: Array<{
+    node: {
+      id: string
+      name: string
+    }
+  }>
+}
 
 export interface PersonAdminCardFragment {
-  id: string,
-  name: string,
-  bio: string | null,
-  photo:  {
-    sourceUrl: string,
-  } | null,
-  twitterHandle: string | null,
-};
+  id: string
+  name: string
+  bio: string | null
+  photo: {
+    sourceUrl: string
+  } | null
+  twitterHandle: string | null
+}
 
 export interface PersonListFragment {
-  edges:  Array< {
-    node:  {
-      id: string,
-      name: string,
-    },
-  } >,
-};
+  edges: Array<{
+    node: {
+      id: string
+      name: string
+    }
+  }>
+}
 
 export interface VenueAdminCardFragment {
-  id: string,
-  name: string,
-  description: string,
-  address:  {
-    streetAddress: string,
-    postcode: string,
-    city: string,
-  },
-  photo:  {
-    sourceUrl: string,
-  } | null,
-};
+  id: string
+  name: string
+  description: string
+  address: {
+    streetAddress: string
+    postcode: string
+    city: string
+  }
+  photo: {
+    sourceUrl: string
+  } | null
+}
 
 export interface VenueListFragment {
-  edges:  Array< {
-    node:  {
-      id: string,
-      name: string,
-    },
-  } >,
-};
+  edges: Array<{
+    node: {
+      id: string
+      name: string
+    }
+  }>
+}
 
 export interface AddressEditorFragment {
-  streetAddress: string,
-  city: string,
-  postcode: string,
-};
+  streetAddress: string
+  city: string
+  postcode: string
+}
 
 export interface EventFeedCardFragment {
-  id: string,
-  name: string,
-  family: EventFamily,
+  id: string
+  name: string
+  family: EventFamily
   // Return the event venue
-  venue:  {
-    id: string,
-    name: string,
-    address:  {
-      postcode: string,
-    },
-  },
-  startTime: string,
-  endTime: string,
-  introduction: string,
-};
+  venue: {
+    id: string
+    name: string
+    address: {
+      postcode: string
+    }
+  }
+  startTime: string
+  endTime: string
+  introduction: string
+}
