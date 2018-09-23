60923d03e44b93b2409e17665cf2f24fe85ecb14
Chris Devereux <devereux.chris@gmail.com>
2018-09-23 19:18:42 +0100

    TWT_ONLY notifications

--- common/domain/ConferenceNotificationScope.ts
+++ common/domain/ConferenceNotificationScope.ts
@@ -1,6 +1,7 @@
 import { never } from 'common/util'
 
 export enum ConferenceNotificationScope {
+  TWT_ONLY = 'TWT_ONLY',
   EVERYONE = 'EVERYONE',
   DELEGATES = 'DELEGATES',
 }
@@ -19,6 +20,9 @@ export function getConferenceNotificationScopeLabel(
   }
   if (scope === ConferenceNotificationScope.DELEGATES) {
     return 'Just Delegates'
+  }
+  if (scope === ConferenceNotificationScope.TWT_ONLY) {
+    return 'Non-delegates'
   }
 
   return never(scope)
